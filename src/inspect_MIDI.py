123115a84305e1e96df5ee200832284dba417f8d
Raghav <raghavraghavkumar18@gmail.com>
2026-02-03 19:46:13 +0530

    add bucketing for monophony

--- src/inspect_MIDI.py
+++ src/inspect_MIDI.py
@@ -3,10 +3,11 @@ import pretty_midi
 midi_path = "/home/raghav/Desktop/practice_code/MelodyTranscriber/outputs/MIDI/Mozart_from_Piano_Sonata_K310_first_movement_basic_pitch.mid"
 
 pm = pretty_midi.PrettyMIDI(midi_path)
-
+# print(pm)
 print(f"Tempo estimate: {pm.estimate_tempo():.2f} BPM")
 print(f"Number of instruments: {len(pm.instruments)}\n")
-
+note_bucket={}
+bucketSize=0.03
 for i, instrument in enumerate(pm.instruments):
     print(f"Instrument {i}:")
     print(f"  Program: {instrument.program}")
@@ -15,12 +16,15 @@ for i, instrument in enumerate(pm.instruments):
 
     for note in instrument.notes:
         duration = note.end - note.start
-        print(
-            f"Pitch: {note.pitch:3d} | "
-            f"Start: {note.start:6.3f}s | "
-            f"End: {note.end:6.3f}s | "
-            f"Dur: {duration:6.3f}s | "
-            f"Vel: {note.velocity}"
-        )
+        note_index=int(note.start/bucketSize)
+        li=list((note.pitch,note.start,note.end,note.velocity))
+        if(note_index not in note_bucket): note_bucket[note_index]=[]
+    
+        note_bucket[note_index].append(li)
+   
 
     print("\n" + "-" * 60 + "\n")
+
+
+    print(note_bucket)
+

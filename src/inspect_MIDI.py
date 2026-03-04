30d9310ed258227d156db256ec4a4f4d8d954a94
raghav <raghav@192.168.1.12>
2026-03-04 17:47:31 +0530

    select confident notes for monotonicity

--- src/inspect_MIDI.py
+++ src/inspect_MIDI.py
@@ -1,9 +1,10 @@
+
 import pretty_midi
 
-midi_path = "/home/raghav/Desktop/practice_code/MelodyTranscriber/outputs/MIDI/Mozart_from_Piano_Sonata_K310_first_movement_basic_pitch.mid"
+midi_path = "outputs/MIDI/Mozart_from_Piano_Sonata_K310_first_movement_basic_pitch.mid"
 
 pm = pretty_midi.PrettyMIDI(midi_path)
-# print(pm)
+
 print(f"Tempo estimate: {pm.estimate_tempo():.2f} BPM")
 print(f"Number of instruments: {len(pm.instruments)}\n")
 note_bucket={}
@@ -27,4 +28,39 @@ for i, instrument in enumerate(pm.instruments):
 
 
     print(note_bucket)
+
+    note_map={}
+
+    for idx, record in note_bucket.items():
+        
+        # for record in note_bucket.items():
+                winner=None
+                for candidate in record:
+                    if winner is None:
+                        winner=candidate
+                    elif candidate[3]>winner[3]:
+                         winner=candidate
+
+                
+                note_map[idx]=winner
+    melody=[]
+    for idx in sorted(note_map.keys()):
+         melody.append(note_map[idx])
+
+    for i in range(len(melody)-1):
+        current_note=melody[i]
+        next_note=melody[i+1]
+
+        if current_note[2]>next_note[1]:
+            current_note[2]=next_note[1]
+         
+    print(len(melody))
+    for note in melody:
+        print(note)
+    
+    # print(note_map)
+
+
+
+
 
